f928ef1f2d515b4ea230937d704d24743ee1eec0
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-01-12 08:53:09 -0600

    Added unit tests and javadoc for ScreenLifecycleManager

--- src/main/java/runner/ScreenLifecycleManager.java
+++ src/main/java/runner/ScreenLifecycleManager.java
@@ -2,6 +2,10 @@ package runner;
 
 import util.Navigator;
 
+/**
+ * This class manages multiple screen life cycles.
+ * This makes sure to start and stop lifecycles.
+ */
 public class ScreenLifecycleManager {
     private final GUI gui;
     private ScreenLifecycle currentScreenLifecycle = null;
@@ -10,6 +14,13 @@ public class ScreenLifecycleManager {
         this.gui = gui;
     }
 
+    /**
+     * This stops the old lifecycle, and starts the new one.
+     * This also updates the gui with the current lifecycle.
+     *
+     * @param screen   the new <code>Screen</code> to manage
+     * @param metadata any data that should be passed to the screen's <code>Model</code>.
+     */
     public void switchTo(Screen screen, Object metadata) {
         ScreenLifecycle createdScreenLifecycle = new ScreenLifecycle(screen, new Navigator(this, ScreenRegistry.getInstance()));
         createdScreenLifecycle.init(metadata);
@@ -21,5 +32,14 @@ public class ScreenLifecycleManager {
 
         currentScreenLifecycle = createdScreenLifecycle;
         gui.switchView(screen.getView());
+    }
+
+    /**
+     * Used internally to clean up lifecycles when the program is stopped.
+     */
+    void cleanUpLifecycles() {
+        if (currentScreenLifecycle != null) {
+            currentScreenLifecycle.cleanup();
+        }
     }
 }
