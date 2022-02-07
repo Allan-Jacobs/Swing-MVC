748860df12fff321818690a4a271933d36e90bfb
Allan-Jacobs <ajjacobs01@kellerisd.com>
2022-02-07 10:45:12 -0600

    Added optional frame configuration at app startup.

---
+++ src/main/java/com/redstoneblocks/java/swing_mvc/util/FrameConfiguration.java
@@ -0,0 +1,8 @@
+package com.redstoneblocks.java.swing_mvc.util;
+
+import com.redstoneblocks.java.swing_mvc.runner.GUI;
+
+public abstract class FrameConfiguration
+{
+    public abstract void config(GUI gui);
+}
