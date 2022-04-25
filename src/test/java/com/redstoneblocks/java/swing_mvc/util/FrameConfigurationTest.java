693f37256c8bf0d7d6fdd1c2cda528e5104919db
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-04-24 20:21:59 -0500

    Fix a bug in a unit test that would fail in an environment without graphics, such as CI

--- src/test/java/com/redstoneblocks/java/swing_mvc/util/FrameConfigurationTest.java
+++ src/test/java/com/redstoneblocks/java/swing_mvc/util/FrameConfigurationTest.java
@@ -1,28 +1,23 @@
 package com.redstoneblocks.java.swing_mvc.util;
 
 import com.redstoneblocks.java.swing_mvc.runner.GUI;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.*;
 
 class FrameConfigurationTest {
-    GUI gui;
-
-    @BeforeEach
-    void setupGUI() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        Constructor<GUI> constructor = GUI.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        gui = constructor.newInstance();
-    }
-
+    @SuppressWarnings("MagicConstant")
     @Test
     void canConfigureGUI() {
+        GUI gui = mock(GUI.class);
+        doCallRealMethod().when(gui).getDefaultCloseOperation();
+        doCallRealMethod().when(gui).setDefaultCloseOperation(anyInt());
+        gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // This is set in the default constructor, but we set it here because we are not calling the real constructor
+
         FrameConfiguration configuration = frame -> frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
         int closeOperation = gui.getDefaultCloseOperation();
