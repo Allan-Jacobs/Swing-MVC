e994598940fd7dcd910611305e5e0108ea58a266
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-04-24 19:14:49 -0500

    Convert FrameConfiguration into an interface

--- src/main/java/com/redstoneblocks/java/swing_mvc/util/FrameConfiguration.java
+++ src/main/java/com/redstoneblocks/java/swing_mvc/util/FrameConfiguration.java
@@ -2,7 +2,6 @@ package com.redstoneblocks.java.swing_mvc.util;
 
 import com.redstoneblocks.java.swing_mvc.runner.GUI;
 
-public abstract class FrameConfiguration
-{
-    public abstract void config(GUI gui);
+public interface FrameConfiguration {
+    void config(GUI gui);
 }
